5938178913d0b3098e77a9d6ba91bccb206748da
August2111 <Info@FlapsOnline.de>
2023-09-28 12:28:19 +0200

    update fw-upgrade.sh - better file selection and handling with interrupts

--- meta-openvario/recipes-apps/ovmenu-ng-skripts/files/fw-upgrade.sh
+++ meta-openvario/recipes-apps/ovmenu-ng-skripts/files/fw-upgrade.sh
@@ -3,10 +3,13 @@
 echo "Firmware Upgrade OpenVario"
 echo "=========================="
 
+DEBUG_STOPS="n"
+
 # USB_STICK=usb
 USB_STICK=/usb/usbstick
 
 DIALOG_CANCEL=1
+SELECTION=/tmp/output.sh.$$
 
 # the OV dirname at USB stick
 OV_DIRNAME=$USB_STICK/openvario
@@ -31,8 +34,8 @@ function select_image(){
     images=$OV_DIRNAME/images/O*V*-*.gz
 
     let i=0 # define counting variable
-    files=() # define working array
-    files_nice=()
+    files=()        # define file array 
+    files_nice=()   # define array with index + file description for dialog
     while read -r line; do # process file by file
         let i=$i+1
         files+=($i "$line")
@@ -48,60 +51,57 @@ function select_image(){
             temp3=$(echo $line | grep -o "testing")
         fi
         
-        # temp="$temp1 $temp2 $temp3"
-        files_nice+=($i "$temp1 $temp2 $temp3")
+        files_nice+=($i "$temp1 $temp2 $temp3") # selection index + name
     done < <( ls -1 $images )
     
-    #=======================================
-    # only for testing
-    echo "curr dir $(pwd)"
-    for i in "${files_nice[@]}"
-    do
-        echo "$i"
-   # or do whatever with individual element of the array
-    done
-    read -rsp $'Press enter to continue...(0)\n'
-    #=======================================
     
     if [ -n "$files" ]; then
         # Search for images
-        FILE=$(dialog --backtitle "Selection upgrade image from file list" \
+        # FILE=$(
+        dialog --backtitle "Selection upgrade image from file list" \
         --title "Select image" \
         --menu "Use [UP/DOWN] keys to move, ENTER to select" \
-        18 60 12 \
-        "${files_nice[@]}" 3>&2 2>&1 1>&3)
+        18 60 12 "${files_nice[@]}" 2> "${SELECTION}"
         
-        IMAGEFILE=$(readlink -f $(ls -1 $images |sed -n "$FILE p"))
+        IMAGEFILE=$(readlink -f $(ls -1 $images |sed -n "$(<${SELECTION}) p"))
     else
-        FILE=""
+        echo "no image file available"
+        IMAGEFILE=""
     fi
 
     if [ ! -e "$IMAGEFILE" ]; then
         dialog --backtitle "${TITLE}" \
         --title "Select image" \
         --msgbox "\n\n No image file found !!" 10 40
+        echo "no image file... 2nd"
         exit
     else
-        echo "selected image file:    $IMAGEFILE"
+        echo "selected image file:    $(basename $IMAGEFILE)"
     fi
     
 }
 
 
 function start_upgrade(){
-    if [ "$HW_CONFIG" == "" ]; then
+    echo "Start Upgrading..."
+    if [ "$HW_CONFIG" = "" ]; then
        HW_CONFIG="ch57"
     fi
     # rename the ov-recovery.itx to ov-recovery.itb for the next step!!!
     cp -f $OV_DIRNAME/images/$HW_CONFIG/ov-recovery.itb    $OV_DIRNAME/ov-recovery.itb
-
+    #August: =============================================
+    echo "Das ist eine Test-Unterbrechung!!!"
+    read INPUT
+    exit
+    #================================
     echo "copy the 1st block (20MB) (boot-sector!)"
     # gzip -cfd $USB_STICK/BootPartition/BootSector16MB.gz | dd of=$TARGET bs=1M
     gzip -cfd ${BOOT_PARTITION} | dd of=$TARGET bs=1M count=20
 
     echo "Boot Recovery preparation with '${IMAGEFILE}' finished!"
-    if [ "$DEBUG_STOPS" == "y" ]; then
+    if [ "$DEBUG_STOPS" = "y" ]; then
       # set outside in shell
+      echo "Debug Stop: Finish"
       read -rsp $'Press enter to continue...\n'
     fi
     shutdown -r now
@@ -115,6 +115,12 @@ select_image
 # Complete Update
 if [ -f "${IMAGEFILE}" ]; then
     echo "Start..."
+    ### #August: =============================================
+    ### echo "Interrupt (after select)!!!"
+    ### # read INPUT
+    ### # -rsp $'Press enter to continue...(start_upgrade)\n'
+    ### exit
+    ### #================================
 
     # make tmp dir clean:
     if [ -d "$SDC_DIR" ]; then
@@ -122,30 +128,8 @@ if [ -f "${IMAGEFILE}" ]; then
         rm -r $SDC_DIR
     fi
 
-    # save boot folder to Backup from partition 1
-    echo "save boot folder to Backup from partition 1"
-    rm -rf $MOUNT_DIR1
-    mkdir -p $BACKUP_DIR/part1
-    mkdir -p $MOUNT_DIR1
-    mount /dev/mmcblk0p1 $MOUNT_DIR1
-    cp -frv $MOUNT_DIR1/* $BACKUP_DIR/part1/
-
-    # save XCSoarData from partition 2:
-    echo "save XCSoarData from partition 2"
-    rm -rf $MOUNT_DIR2
-    mkdir -p $MOUNT_DIR2
-    mkdir -p $BACKUP_DIR/part2/xcsoar
-    mount /dev/mmcblk0p2 $MOUNT_DIR2
-    cp -frv $MOUNT_DIR2/home/root/.xcsoar/* $BACKUP_DIR/part2/xcsoar/
-    
-    if [ -d "$MOUNT_DIR2/home/root/.glider_club" ]; then
-        echo "save gliderclub data from partition 2"
-        mkdir -p $BACKUP_DIR/part2/glider_club
-        cp -frv $MOUNT_DIR2/home/root/.glider_club/* $BACKUP_DIR/part2/glider_club/
-    fi
-    
     #================== System Config =======================================================
-    #save system config in config.uSys for restoring reason
+    # 1st) save system config in config.uSys for restoring reason
     echo "" > $BACKUP_DIR/config.uSys
     if /bin/systemctl --quiet is-enabled dropbear.socket; then
         echo "SSH=\"enabled\""
@@ -160,8 +144,6 @@ if [ -f "${IMAGEFILE}" ]; then
 
     echo "BRIGHTNESS=\"$(</sys/class/backlight/lcd/brightness)\""
     echo "BRIGHTNESS=\"$(</sys/class/backlight/lcd/brightness)\"" >> $BACKUP_DIR/config.uSys
-    # echo "ROTATION=\"$(</sys/class/graphics/fbcon/rotate_all)\""
-    # echo "ROTATION=\"$(</sys/class/graphics/fbcon/rotate_all)\"" >> $BACKUP_DIR/config.uSys
     
     source $MOUNT_DIR1/config.uEnv
     case $(basename $fdtfile .dtb) in
@@ -180,13 +162,40 @@ if [ -f "${IMAGEFILE}" ]; then
     echo "ROTATION=\"$rotation\"" >> $BACKUP_DIR/config.uSys
     # TEMP=$(grep "rotation" /boot/config.uEnv)
 
+    # 2nd) save boot folder to Backup from partition 1
+    echo "save boot folder to Backup from partition 1"
+    rm -rf $MOUNT_DIR1
+    mkdir -p $BACKUP_DIR/part1
+    mkdir -p $MOUNT_DIR1
+    mount /dev/mmcblk0p1 $MOUNT_DIR1
+    # cp -frv $MOUNT_DIR1/* $BACKUP_DIR/part1/
+    rsync -auv --progress $MOUNT_DIR1/* $BACKUP_DIR/part1/
+    #      --exclude ...
+
+    # 3rd) save XCSoarData from partition 2:
+    echo "save XCSoarData from partition 2"
+    rm -rf $MOUNT_DIR2
+    mkdir -p $MOUNT_DIR2
+    mkdir -p $BACKUP_DIR/part2/xcsoar
+    mount /dev/mmcblk0p2 $MOUNT_DIR2
+    # cp -frv $MOUNT_DIR2/home/root/.xcsoar/* $BACKUP_DIR/part2/xcsoar/
+    rsync -auv --progress $MOUNT_DIR2/home/root/.xcsoar/* $BACKUP_DIR/part2/xcsoar/ \
+          --exclude cache  --exclude logs
+    
+    if [ -d "$MOUNT_DIR2/home/root/.glider_club" ]; then
+        echo "save gliderclub data from partition 2"
+        mkdir -p $BACKUP_DIR/part2/glider_club
+        cp -frv $MOUNT_DIR2/home/root/.glider_club/* $BACKUP_DIR/part2/glider_club/
+    fi
+    
+
     # Synchronize the commands (?)
     sync
 
-
     # pause:
-    if [ "$DEBUG_STOPS" == "y" ]; then
+    if [ "$DEBUG_STOPS" = "y" ]; then
       # set outside in shell
+      echo "Debug Stop: After Saving"
       read -rsp $'Press enter to continue...\n'
     fi
     umount /dev/mmcblk0p1
@@ -210,22 +219,31 @@ if [ -f "${IMAGEFILE}" ]; then
     rm -rf $MNT_DIR
 
     # echo "Upgrade with '${IMAGEFILE}'"
-    if [ "$DEBUG_STOPS" == "y" ]; then
+    if [ "$DEBUG_STOPS" = "y" ]; then
       # set outside in shell
+      echo "Debug Stop: After Upgrade Step 1"
       read -rsp $'Press enter to continue...(4)\n'
     fi
     IMAGENAME=$(basename "$IMAGEFILE" .gz)
     TIMEOUT=5
-    dialog --nook --nocancel --pause "OpenVario Upgrade with \\n'${IMAGENAME}' ... \\n Press [ESC] for interrupting" 20 60 $TIMEOUT 2>&1
-
-    # Synchronize the commands (?)
-    sync
-
-    case $? in
-        0) start_upgrade;;
-        *) echo "Upgrade interrupted!";;
+    # DIALOG_CANCEL=1 
+    dialog --nook --nocancel --pause \
+    "OpenVario Upgrade with \\n'${IMAGENAME}' ... \\n Press [ESC] for interrupting" \
+    20 60 $TIMEOUT 2>&1
+    # DO NOTHING AFTER USING '$?' ONE TIMES!!!
+    
+    # store the selection for debug reasons:
+    INPUT="$?"
+    case $INPUT in
+        0) 
+            start_upgrade;;
+        *) echo "Upgrade interrupted!" ;;
     esac
 
 else
-    echo "'${IMAGEFILE}' don't exist, no recovery!"
+    if [ "${IMAGEFILE}" = "" ]; then
+        echo "IMAGEFILE is empty, no recovery!"
+    else
+        echo "'${IMAGEFILE}' don't exist, no recovery!"
+    fi
 fi
