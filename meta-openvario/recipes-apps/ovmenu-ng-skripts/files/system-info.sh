ead2b56856ddc420b8791bcb4b6aa7e7d797da4a
August2111 <Info@FlapsOnline.de>
2023-11-06 13:46:25 +0100

    system-info.sh: remove information with lower general interest
    maybe move it to a new 2nd page

--- meta-openvario/recipes-apps/ovmenu-ng-skripts/files/system-info.sh
+++ meta-openvario/recipes-apps/ovmenu-ng-skripts/files/system-info.sh
@@ -4,20 +4,21 @@
 # System info script gives an overview which packages and which versions are instaled
 #
 # Created by Blaubart,        2022-02-08
+# Changed by August2111,      2023-11-07
 #
 
 # collect info of system and more installed packages
 IMAGE_VERSION=$(cat /etc/os-release | grep VERSION_ID | cut -d '=' -f 2)
 KERNEL_VERSION=$(uname -r)
-XCSOAR_MAPS_VERSION=$(opkg list-installed xcsoar-maps* | cut -d '-' -f 4)
+# aug deletet XCSOAR_MAPS_VERSION=$(opkg list-installed xcsoar-maps* | cut -d '-' -f 4)
 XCSOAR_MENU=$(opkg list-installed xcsoar-menu* | cut -d '-' -f 3)
 IP_ETH0=$(ip route | grep eth0 | head -n 1 | cut -d ' ' -f 8)
 MAC=`ip li|grep -A 1 eth0|tail -n 1|cut -d ' ' -f 6`
 HOSTNAME=$(cat /etc/hostname)
 IP_WLAN=$(ip route | grep wlan0 | head -n 1| cut -d ' ' -f 8)
-I2C_TOOLS=$(opkg list-installed i2c-tools | cut -d '-' -f 3)
-E2FSPROGS=$(opkg list-installed e2fsprogs | cut -d '-' -f 2)
-USB_MODESWITCH=$(opkg list-installed usb-modeswitch | cut -d '-' -f 3)
+# August2111: I2C_TOOLS=$(opkg list-installed i2c-tools | cut -d '-' -f 3)
+# August2111: E2FSPROGS=$(opkg list-installed e2fsprogs | cut -d '-' -f 2)
+# August2111: USB_MODESWITCH=$(opkg list-installed usb-modeswitch | cut -d '-' -f 3)
 
 # collect status of SSH, variod and sensord
 if  /bin/systemctl --quiet is-enabled dropbear.socket
@@ -49,13 +50,13 @@ echo ' Kernel: '$KERNEL_VERSION
 if [ -n "$(opkg list-installed xcsoar-testing)" ]
 then
 	XCSOAR_VERSION=$(opkg list-installed xcsoar-testing | cut -d ' - ' -f 3)
-	echo ' XCSoar-testing: '$XCSOAR_VERSION
+	echo " XCSoar: '$XCSOAR_VERSION' (testing)"
 else
 	XCSOAR_VERSION=$(opkg list-installed xcsoar         | cut -d '-'   -f 2)
-	echo ' XCSoar:'$XCSOAR_VERSION
+	echo " XCSoar: '$XCSOAR_VERSION'"
 fi
 
-echo ' Maps:'$XCSOAR_MAPS_VERSION
+# echo ' Maps:'$XCSOAR_MAPS_VERSION
 echo ' Menu:'$XCSOAR_MENU
 
 if [ -n "$(opkg list-installed sensord-testing)" ] 
@@ -80,11 +81,12 @@ echo ' IP eth0: '$IP_ETH0
 echo ' MAC-address eth0: '$MAC
 echo ' Hostname: '$HOSTNAME
 echo ' IP wlan0: '$IP_WLAN
-echo -e '\n'
-echo ' supplementary packages that are not included\n in every image:'
-echo ' i2c-tools:'$I2C_TOOLS
-echo ' e2fsprogs:'$E2FSPROGS
-echo ' usb-modeswitch:'$USB_MODESWITCH
+# August2111: remove information with lower general interest
+# August2111: echo -e '\n'
+# August2111: echo ' supplementary packages that are not included\n in every image:'
+# August2111: echo ' i2c-tools:'$I2C_TOOLS
+# August2111: echo ' e2fsprogs:'$E2FSPROGS
+# August2111: echo ' usb-modeswitch:'$USB_MODESWITCH
 echo -e '\n'
 echo ' Status of SSH, variod and sensord:'
 echo ' SSH is '$SSH_STATUS
