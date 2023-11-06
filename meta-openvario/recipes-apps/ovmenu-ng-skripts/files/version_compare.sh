7a6fcb24940bf05cafcb4f4975dcd283d6e686b6
August2111 <Info@FlapsOnline.de>
2023-11-06 13:35:14 +0100

    version_compare.sh - complete rework for a better check of OpenVario revisions
    f.e. compare 17119 and 3.2.20 too

--- meta-openvario/recipes-apps/ovmenu-ng-skripts/files/version_compare.sh
+++ meta-openvario/recipes-apps/ovmenu-ng-skripts/files/version_compare.sh
@@ -1,28 +1,35 @@
 vercomp () {
+    # echo "$1 vs. $2"
+    # echo "-------------"
     if [[ $1 == $2 ]]
     then
-        return 0
+        return 0 # equal
     fi
     local IFS=.
-    local i ver1=($1) ver2=($2)
-    # fill empty fields in ver1 with zeros
-    for ((i=${#ver1[@]}; i<${#ver2[@]}; i++))
+    local i ver1 ver2
+    # replace '-' with '.' and split it an array
+    read -r -a ver1 <<< ${1//"-"/"."}
+    read -r -a ver2 <<< ${2//"-"/"."}
+    for ((i=0; i<4; i++))
     do
-        ver1[i]=0
+        # fill empty fields in ver1 with zeros
+        if [ -z ${ver1[i]} ]; then ver1[i]=0; fi
+        if [ -z ${ver2[i]} ]; then ver2[i]=0; fi
     done
     
-    if [[ ${ver1[0]} > 10000 ]]
+    if (( ${#ver1[0]} > 4 ))
     then
        # with fw 17119 make version 0.17119 for a correct compare
-            ver1[1]=ver1[0]
+            ver1[1]=${ver1[0]}
             ver1[0]=0
     fi
-    if [[ ${ver2[0]} > 10000 ]]
+    if (( ${#ver2[0]} > 4 ))
     then
        # with fw 17119 make version 0.17119 for a correct compare
-            ver2[1]=ver2[0]
+            ver2[1]=${ver2[0]}
             ver2[0]=0
     fi
+
     for ((i=0; i<${#ver1[@]}; i++))
     do
         if [[ -z ${ver2[i]} ]]
@@ -30,14 +37,24 @@ vercomp () {
             # fill empty fields in ver2 with zeros
             ver2[i]=0
         fi
-        if ((10#${ver1[i]} > 10#${ver2[i]}))
+        if ((${ver1[i]} > ${ver2[i]}))
         then
-            return 1
+            return 2 # greater 
         fi
-        if ((10#${ver1[i]} < 10#${ver2[i]}))
+        if ((${ver1[i]} < ${ver2[i]}))
         then
-            return -1
+            return 1 # lower
         fi
     done
-    return 0
+    return 0 # equal
 }
+
+function compare_print(){
+  case $1 in
+    0) echo "Version is identical!";;
+    1) echo "Version is lower!";;
+    2) echo "Version is greater!";;
+    *) echo "Version compare error!";;
+  esac
+}
+
