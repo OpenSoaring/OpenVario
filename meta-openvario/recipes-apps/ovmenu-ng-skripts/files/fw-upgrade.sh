25a41bddc608dfd0e499d72977994a7eed5b28c7
August2111 <Info@FlapsOnline.de>
2023-11-11 08:14:37 +0100

    fw-upgrade.sh - insert function vercomp() from version_compare.sh
    because in older firmware (< 3.2.20) this script isn't available

--- meta-openvario/recipes-apps/ovmenu-ng-skripts/files/fw-upgrade.sh
+++ meta-openvario/recipes-apps/ovmenu-ng-skripts/files/fw-upgrade.sh
@@ -44,7 +44,56 @@ function debug_stop(){
 
 BATCH_PATH=$(dirname $0)
 echo "Batch Path = '$BATCH_PATH'"
-source $BATCH_PATH/version_compare.sh
+vercomp () {
+    # echo "$1 vs. $2"
+    # echo "-------------"
+    if [[ $1 == $2 ]]
+    then
+        return 0 # equal
+    fi
+    local IFS=.
+    local i ver1 ver2
+    # replace '-' with '.' and split it an array
+    read -r -a ver1 <<< ${1//"-"/"."}
+    read -r -a ver2 <<< ${2//"-"/"."}
+    for ((i=0; i<4; i++))
+    do
+        # fill empty fields in ver1 with zeros
+        if [ -z ${ver1[i]} ]; then ver1[i]=0; fi
+        if [ -z ${ver2[i]} ]; then ver2[i]=0; fi
+    done
+    
+    if (( ${#ver1[0]} > 4 ))
+    then
+       # with fw 17119 make version 0.17119 for a correct compare
+            ver1[1]=${ver1[0]}
+            ver1[0]=0
+    fi
+    if (( ${#ver2[0]} > 4 ))
+    then
+       # with fw 17119 make version 0.17119 for a correct compare
+            ver2[1]=${ver2[0]}
+            ver2[0]=0
+    fi
+
+    for ((i=0; i<${#ver1[@]}; i++))
+    do
+        if [[ -z ${ver2[i]} ]]
+        then
+            # fill empty fields in ver2 with zeros
+            ver2[i]=0
+        fi
+        if ((${ver1[i]} > ${ver2[i]}))
+        then
+            return 2 # greater 
+        fi
+        if ((${ver1[i]} < ${ver2[i]}))
+        then
+            return 1 # lower
+        fi
+    done
+    return 0 # equal
+}
 
 function select_image(){
     images=$OV_DIRNAME/images/O*V*-*.gz
