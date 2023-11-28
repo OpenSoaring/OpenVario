8a82ae40a51e4a6bb2c07e1ff3f64d27d360112c
August2111 <Info@FlapsOnline.de>
2023-11-28 11:55:10 +0100

    update-system-config.sh - changed location of upgrade.cfg

--- meta-openvario/recipes-apps/ovmenu-ng-skripts/files/update-system-config.sh
+++ meta-openvario/recipes-apps/ovmenu-ng-skripts/files/update-system-config.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-if [ -e "./upgrade.cfg" ]
-then
+HOME=/home/root
+
+if [ -e "$HOME/recover_data/upgrade.cfg" ]; then
     echo "'upgrade.cfg' exist!!"
-    source ./upgrade.cfg
+    source $HOME/recover_data/upgrade.cfg
     
     # ==================== SSH ===============================
     if [ ! "$SSH" == "" ]; then 
@@ -27,8 +28,39 @@ then
         echo "$BRIGHTNESS" > /sys/class/backlight/lcd/brightness
         echo " [####======] Brightness set to '$BRIGHTNESS'"
     fi
+    # ==================== Recover Data ======================
+    if [ "$UPGRADE_TYPE" == "2" ]; then 
+      if [ ! -d $DATADIR/OpenSoarData ]; then
+        # the data dir is new and has to be filled from USB, this stick should be still 
+        # available after upgrade
+        # This is only the case on an upgrade from old to a new system
+        mkdir -p $DATADIR/OpenSoarData
+        echo "'data/OpenSoarData'is new and has to be filled..."  >> $DEBUG_LOG
+      fi
+      if [ ! -d $DATADIR/XCSoarData ]; then
+        mkdir -p $DATADIR/OpenSoarData
+        echo "'data/XCSoarData'is new and has to be filled..."  >> $DEBUG_LOG
+      fi
+      file_count=$(find $HOME/.xcsoar -maxdepth 1 -type f | wc -l)
+      if (( $file_count > 6 )); then
+        rsync -ruvtcE --progress $HOME/.xcsoar/* $DATADIR/OpenSoarData/ \
+                    --delete --exclude cache  --exclude logs
+        echo "rsync from .xcsoar to $DATADIR/OpenSoarData"  >> $DEBUG_LOG
+
+        rsync -ruvtcE --progress $HOME/.xcsoar/* $DATADIR/XCSoarData/ \
+                      --delete --exclude cache  --exclude logs
+        sync
+        echo "rsync from .xcsoar to $DATADIR/XCSoarData"  >> $DEBUG_LOG
+      fi
+      rm -rvf $HOME/.xcsoar/*
+      if [ -d $HOME/.glider_club ]; then
+        mkdir -p $DATADIR/.glider_club
+        rsync -ruvtcE --progress $HOME/.glider_club/* $DATADIR/.glider_club/
+        sync
+        rm -rvf $HOME/.glider_club
+      fi
+    fi
     # ==================== End ===============================
-    mv ./upgrade.cfg ./_upgrade.cfg
-    exit
+    mv $HOME/recover_data/upgrade.cfg $HOME/recover_data/_upgrade.cfg
 fi
 
