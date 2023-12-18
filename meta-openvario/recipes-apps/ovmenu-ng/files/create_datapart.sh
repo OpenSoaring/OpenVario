94ab8d931919f2fadfb4b7dbc9a30c3700e80517
August2111 <Info@FlapsOnline.de>
2023-12-18 18:29:47 +0100

    create_datapart.sh - spelling error in script (+ cleanup)

--- meta-openvario/recipes-apps/ovmenu-ng/files/create_datapart.sh
+++ meta-openvario/recipes-apps/ovmenu-ng/files/create_datapart.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
-PARTITION_START3=2359296
+# Start at 1.2GB:
+PARTITION3_START=2359296
+# End at default (= maximum):
+PARTITION3_END=
 
 fdisk /dev/mmcblk0 << EOF
 p
 n
 p
 3
-$PARTITION3_START
-
-
+${PARTITION3_START}
+${PARTITION3_END}
 p
 w
 EOF
