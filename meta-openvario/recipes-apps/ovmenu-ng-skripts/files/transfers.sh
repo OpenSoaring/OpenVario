0d6de0d9971c75c6a99ee22916736c0af590c16b
August2111 <Info@FlapsOnline.de>
2023-12-20 17:25:13 +0100

    transfers.sh - rework with better selection and clearer comments

--- meta-openvario/recipes-apps/ovmenu-ng-skripts/files/transfers.sh
+++ meta-openvario/recipes-apps/ovmenu-ng-skripts/files/transfers.sh
@@ -1,60 +1,99 @@
 #!/bin/sh
 
 # Transfer script for Up/Downloadng data to usbstick
+# echo "Transfer to and from OpenVario and USB Stick"
 
-USB_PATH="/usb/usbstick/openvario/"
-OPENSOAR_PATH="/home/root/data/OpenSoarData"
+USB_PATH="/usb/usbstick/openvario"
+TRANSFER_OPTION=$1
+MAIN_APP=$2
 
 RSYNC_OPTION=""
 DATA_FOLDER="OpenSoarData"
+if [ "$MAIN_APP" = "xcsoar" ]; then
+  $DATA_FOLDER="XCSoarData"
+fi
+SOAR_DATA_PATH="/home/root/data/$DATA_FOLDER"
+# echo "MAIN-APPLICATION: '$MAIN_APP'"
 
 case "$TRANSFER_OPTION" in
-	'download-data')
+    download-igc)
+    # ----------
         echo "Syncronizing $DATA_FOLDER with USB "
-		SRC_PATH="$OPENSOAR_PATH"
-		DEST_PATH="$USB_PATH/download/$DATA_FOLDER"
-		;;
-	'upload-data')
+        SRC_PATH="$SOAR_DATA_PATH"
+        DEST_PATH="$USB_PATH/download/$DATA_FOLDER"
+        mkdir -p $DEST_PATH
+        ;;
+    download-data)
+    # ----------
+        echo "Syncronizing $DATA_FOLDER with USB "
+        SRC_PATH="$SOAR_DATA_PATH"
+        DEST_PATH="$USB_PATH/download/$DATA_FOLDER"
+        mkdir -p $DEST_PATH
+        ;;
+    upload-data)
+    # ----------
         echo "Copy USB data to $DATA_FOLDER"
-		SRC_PATH="$USB_PATH/upload/$DATA_FOLDER"
-		DEST_PATH="$OPENSOAR_PATH"
-		;;
-	'sync-data' )
+        SRC_PATH="$USB_PATH/upload/$DATA_FOLDER"
+        if [ ! -d $SRC_PATH ] && [ "$MAIN_APP" = "OpenSoar" ]; then
+          echo "Source path does not exist"
+          echo "  $SRC_PATH"
+          SRC_PATH=$USB_PATH/upload/XCSoarData
+        fi
+        if [ ! -d $SRC_PATH ]; then
+          echo "Source path does not exist"
+          echo "  $SRC_PATH"
+          SRC_PATH=$USB_PATH/upload/xcsoar
+        fi
+        DEST_PATH="$SOAR_DATA_PATH"
+        ;;
+    sync-data)
+    # ----------
         echo "Syncronizing USB data with $DATA_FOLDER"
-		SRC_PATH="$USB_PATH/upload/$DATA_FOLDER"
-		DEST_PATH="$OPENSOAR_PATH"
-        RSYNC_OPTION="--delete"; fi
-	'upload-all')
+        SRC_PATH="$USB_PATH/upload/$DATA_FOLDER"
+        DEST_PATH="$SOAR_DATA_PATH"
+        RSYNC_OPTION="--delete"
+        ;;
+    upload-all)
+    # ----------
         echo "Syncronizing complete USB data folder 'upload' with data partition"
-		SRC_PATH="$USB_PATH/upload"
-		DEST_PATH="$OPENSOAR_PATH"
-		;;
-	*)
-		>&2 echo 'transfer option unknown!'
-		exit 1
+        SRC_PATH="$USB_PATH/upload"
+        DEST_PATH="$SOAR_DATA_PATH"
+        ;;
+    *)
+    # ----------
+        echo "*** ERROR ***!"
+        echo 'transfer option '$TRANSFER_OPTION' unknown!'
+        exit 1
 esac
 
-if [ ! -d "$SRC_PATH" ] || [ ! -d "$DEST_PATH" ]; then
-	>&2 echo "Source $SRC_PATH or destination path $DEST_PATH does not exist"
-	exit 1
+echo "Transfer-Option: $TRANSFER_OPTION" 
+echo "Source:         '$SRC_PATH'"
+echo "Destination:    '$DEST_PATH'"
+
+if [ ! -d "$SRC_PATH" ]; then
+    echo "*** ERROR ***!"
+    echo "Source path does not exist!"
+    exit 1
+elif [ ! -d "$DEST_PATH" ]; then
+    echo "*** ERROR ***!"
+    echo "Destination path does not exist!"
+    exit 1
 fi
 
 if [ -z "$(find "$SRC_PATH" -type f | head -n 1 2>/dev/null)" ]; then
-	echo 'No files found !!!'
+    echo 'No files found !!!'
 else
-    echo "Transfer-Cmd:   '$TRANSFER_OPTION'"
-    echo "Source:         '$SRC_PATH'"
-    echo "Destination:    '$DEST_PATH'"
   # We use -c here due to cubieboards not having an rtc clock
-	if rsync -rc --progress $RSYNC_OPTION "${SRC_PATH}/" "$DEST_PATH/"; then
-		echo 'All files transfered successfully.'
-	else
-		>&2 echo 'An error has occured!'
-		exit 1
-	fi
+    if rsync -rc --progress $RSYNC_OPTION "${SRC_PATH}/" "$DEST_PATH/"; then
+        echo 'All files transfered successfully.'
+        echo '----------------------------------'
+    else
+        echo "*** ERROR ***!"
+        echo 'An error has occured!'
+        exit 1
+    fi
 fi
 
 # Sync the buffer to be sure data is on disk
 sync
-RSYNC_OPTION=""
 echo 'Done !!'
