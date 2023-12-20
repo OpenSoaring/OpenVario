7800334c6fe81a98626025ff06bfa6759151a5b9
August2111 <Info@FlapsOnline.de>
2023-12-20 17:27:33 +0100

    download-igc.sh - switch from .xcsoar to OpenSoarData
    remove the umount at the end

--- meta-openvario/recipes-apps/ovmenu-ng-skripts/files/download-igc.sh
+++ meta-openvario/recipes-apps/ovmenu-ng-skripts/files/download-igc.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
+# download in /logs only?
+DATA_FOLDER="OpenSoarData"
+if [ "$MAIN_APP" = "xcsoar" ]; then
+  $DATA_FOLDER="XCSoarData"
+fi
+SOAR_DATA_PATH="/home/root/data/$DATA_FOLDER"
+
+IGC_PATH="$SOAR_DATA_PATH/logs/"
+
 USB_PATH="/usb/usbstick/openvario/igc"
-IGC_PATH="/home/root/.xcsoar/logs/"
-mkdir -p $USB_PATH
+mkdir -p $USB_PATH    # if not exists
 
 # trap and delete temp files
 trap "rm /tmp/menuitem.$$;rm /tmp/tail.$$; exit" SIGHUP SIGINT SIGTERM EXIT
@@ -16,36 +24,30 @@ filter()
     done
 }
 
-
+#---------------------------------------------------------------------------
 LINES=$(filter)
 
 IFS=$'\n\t'
 
-if [ -d $IGC_PATH ];
-  then
-
-    dialog --begin 3 4 --backtitle "OpenVario" --title "Download IGC" --menu "Move using [UP] [DOWN],[Enter] to Select" 22 50 20 $LINES 2>/tmp/menuitem.$$
+if [ -d $IGC_PATH ]; then
+    dialog --begin 3 4 --backtitle "OpenVario" --title "Download IGC" \
+    --menu "Move using [UP] [DOWN],[Enter] to Select" 22 50 20 $LINES \
+    2>/tmp/menuitem.$$
     if [ $? -eq 0 ] ; then
-
-	echo "Downloading File">/tmp/tail.$$
-	ITEM=$(cat /tmp/menuitem.$$)
-	FILE=$(echo "$LINES" | sed -n 's/\t'$ITEM'\t//p')
-	cp $IGC_PATH/$FILE $USB_PATH/$FILE 2>>/tmp/tail.$$
-	echo $FILE >>/tmp/tail.$$
-	echo "Done !!" >> /tmp/tail.$$
-
+      echo "Downloading File">/tmp/tail.$$
+      ITEM=$(cat /tmp/menuitem.$$)
+      FILE=$(echo "$LINES" | sed -n 's/\t'$ITEM'\t//p')
+      cp $IGC_PATH/$FILE $USB_PATH/$FILE 2>>/tmp/tail.$$
+      echo $FILE >>/tmp/tail.$$
+      echo "Done !!" >> /tmp/tail.$$
     else
-
-	echo "User Cancel">/tmp/tail.$$
+      echo "User Cancel">/tmp/tail.$$
     fi
-
 else
-
     echo "Directory not Found !!">/tmp/tail.$$
 fi
 
 unset IFS
 
 dialog --begin 3 4 --backtitle "OpenVario" --title "Download IGC" --tailbox /tmp/tail.$$ 22 50
-umount /dev/sda1
 
