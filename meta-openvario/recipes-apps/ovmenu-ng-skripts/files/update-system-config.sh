61f3da55a35d11772c628505cca7b198c5a86a41
August2111 <Info@FlapsOnline.de>
2023-12-30 16:38:20 +0100

    update-system-config.sh - bugfix restoring daemons (SSH vs. dropbear.socket!)

--- meta-openvario/recipes-apps/ovmenu-ng-skripts/files/update-system-config.sh
+++ meta-openvario/recipes-apps/ovmenu-ng-skripts/files/update-system-config.sh
@@ -6,39 +6,23 @@ if [ -e "$HOME/recover_data/upgrade.cfg" ]; then
     echo "'upgrade.cfg' exist!!"
     source $HOME/recover_data/upgrade.cfg
     
-    # ==================== SSH ===============================
-    if [ ! "$SSH" == "" ]; then 
-        echo "SSH            = $SSH"
-        case "$SSH" in
-        enabled)
-            /bin/systemctl  enable --quiet --now dropbear.socket
-            /bin/systemctl   start --quiet --now dropbear.socket
-            echo " [####======] SSH has been enabled permanently.";;
-        temporary)
-            /bin/systemctl disable --quiet --now dropbear.socket
-            /bin/systemctl   start --quiet --now dropbear.socket
-            echo " [####======] SSH has been enabled temporarily.";;
-        disabled)
-            /bin/systemctl  disable --quiet --now dropbear.socket
-            echo " [####======] SSH has been disabled.";;
-        esac
-    fi
     # ==================== Brightness ========================
     if [ ! "$BRIGHTNESS" = "" ]; then 
       echo "$BRIGHTNESS" > /sys/class/backlight/lcd/brightness
       echo " [####======] Brightness set to '$BRIGHTNESS'"
     fi
-    # ==================== Daemon variod ========================
+    # ==================== SSH ===============================
+    if [ -n $SSH ]; then dropbear.socket=$SSH; fi
+    # =========== Daemons variod, sensord (and dropbear.socket) ====
     # Restore variod and sensord status 
-    # SSH_DAEMON=dropbear.socket
-    for DAEMON in variod sensord $SSH_DAEMON
-      case $DAEMON in
-      enabled)  /bin/systemctl  enable --quiet --now $DAEMON
-                /bin/systemctl   start --quiet --now $DAEMON
-                echo " [#####=====] $DAEMON has been enabled."
+    for daemon in variod sensord dropbear.socket
+      case $daemon in
+      enabled)  /bin/systemctl  enable --quiet --now $daemon
+                /bin/systemctl   start --quiet --now $daemon
+                echo " [#####=====] $daemon has been enabled."
                 ;;
-      disabled) /bin/systemctl disable --quiet --now $DAEMON
-                echo " [#####=====] $DAEMON has been disabled."
+      disabled) /bin/systemctl disable --quiet --now $daemon
+                echo " [#####=====] $daemon has been disabled."
                 ;;
       esac
     done
