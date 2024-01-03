6ca938c1f49e309e97131eaa72f47691eea3ff82
August2111 <Info@FlapsOnline.de>
2024-01-03 08:34:58 +0100

    ovmenu-ng.sh - final cleanup

--- meta-openvario/recipes-apps/ovmenu-ng/files/ovmenu-ng.sh
+++ meta-openvario/recipes-apps/ovmenu-ng/files/ovmenu-ng.sh
@@ -1,40 +1,13 @@
 #!/bin/bash
 
-#Config
 DEBUG_STOP="No"
-VERBOSE="Yes"
-WITH_TESTSTEPS="No"
 
-TIMEOUT=0
 INPUT=/tmp/menu.sh.$$
-DIALOG_CANCEL=1
-## HOME=/home/root
 DATADIR=$HOME/data
 USB_STICK=/usb/usbstick
 USB_OPENVARIO=$USB_STICK/openvario
 RECOVER_DIR=$HOME/recover_data
-
 DEBUG_LOG=$HOME/start-debug.log
-#------------------------------------------------------------------------------
-function TestStep() {
-  if [ "$WITH_TESTSTEPS" = "Yes" ]; then
-    echo "Test step:  $1"
-    echo "Test step:  $1"  >> $DEBUG_LOG
-  fi
-}
-#------------------------------------------------------------------------------
-function error_stop() {
-    echo "Error-Stop: $1"
-    read -p "Press enter to continue"
-}
-
-#------------------------------------------------------------------------------
-function printv() {
-    if [ "$VERBOSE" = "Yes" ]; then
-      echo "$1"
-      echo "$1" >> $DEBUG_LOG
-    fi
-}
 
 #------------------------------------------------------------------------------
 function debug_stop() {
@@ -42,18 +15,6 @@ function debug_stop() {
       echo "Debug-Stop: $1"
       read -p "Press enter to continue"
     fi
-}
-
-#------------------------------------------------------------------------------
-function system_check() {
-  printv "system_check"
-  echo "System Check OpenVario"
-  echo "======================"
-  # in the beginning check the complete system
-  # * check partition 2, should never filled about 400-450MB (if 468MB)
-  # * check partition 3 how much free memory is available
-  # * check if usb is in and check the size...
-  # is there a possibility to create an event for insert and remove of the usb?
 }
 
 #=========================================================================
@@ -79,10 +40,8 @@ fi
 echo "begin startup.."
 echo "===============" 
 chmod -Rf 757 $HOME
-TestStep  PreStart
 mv $HOME/start-debug-1.log $HOME/start-debug-2.log
 mv $DEBUG_LOG $HOME/start-debug-1.log
-TestStep  0
 source /boot/config.uEnv
 if [[ -z "$brightness" ]]; then 
   brightness=10
@@ -90,22 +49,22 @@ fi
 echo "$brightness" >/sys/class/backlight/lcd/brightness
 debug_stop "set brightness to '$brightness'"
 
+#------------------------------------------------------------------------------
 # set system configs if upgrade.cfg is available (from Upgrade)
-TestStep  0
 cd $HOME
-if [ -f $HOME/recover_data/upgrade.cfg ]; then
+if [ -f $RECOVER_DIR/upgrade.cfg ]; then
   echo "Update system config" > sysconfig.txt
   /usr/bin/update-system-config.sh
-elif [ ! -f $HOME/recover_data/_upgrade.cfg ]; then
+elif [ ! -f $RECOVER_DIR/_upgrade.cfg ]; then
   echo "upgrade.cfg not found" > sysconfig.txt
 else
   echo "only backup config found !!!!!" > sysconfig.txt
 fi
 
+#------------------------------------------------------------------------------
 echo "begin startup.. $(date %Y-%m-%d %H:%M:%S)" >> $DEBUG_LOG
 DATESTRING=$(date %Y-%m-%d %H:%M:%S)
 date %Y-%m-%d %H:%M:%S >> $DEBUG_LOG
-TestStep  1
 echo "=================================" >> $DEBUG_LOG
 if [ ! -e /dev/mmcblk0p3 ]; then
   echo "/dev/mmcblk0p3 don't exist " >> $DEBUG_LOG
@@ -127,9 +86,8 @@ if [ ! -e /dev/mmcblk0p3 ]; then
   fi
 fi
 
-TestStep  2
+#------------------------------------------------------------------------------
 # Mount the 3rd partition to the data dir
-# if [ ! -d $DATADIR ]; then mkdir $DATADIR; fi
 mount /dev/mmcblk0p3 $DATADIR
 if [ ! -d $DATADIR/OpenSoarData ]; then
   if ! mount /dev/mmcblk0p3 $DATADIR; then
@@ -143,32 +101,6 @@ if [ ! -d $DATADIR/OpenSoarData ]; then
 else
   echo "mmcblk0p3 is mounted at '$DATADIR'"  >> $DEBUG_LOG
 fi
-
-TestStep  5
-if [ -e $DATADIR/.glider_club/GliderClub_Std.prf ]; then
-  MENU_VERSION="club"
-else
-  MENU_VERSION="normal"
-fi
-
-TestStep "6 - 'main_app'"
-# detect main application and start this
-export MAIN_APP=$main_app
-case "$main_app" in 
-  xcsoar)
-      START_PROGRAM="start_xcsoar"
-      ;;
-  OpenSoar|*)
-    if [ "$MENU_VERSION" = "club" ]; then
-      START_PROGRAM="start_opensoar_club"
-    else
-      START_PROGRAM="start_opensoar"
-    fi
-    ;;
-esac
-  # LK8000)  START_PROGRAM="start_lk8000";;
-
-TestStep "7 - 'START_PROGRAM'"
 
 # trap and delete temp files
 trap "rm $INPUT;rm /tmp/tail.$$; exit" SIGHUP SIGINT SIGTERM
